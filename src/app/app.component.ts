80d986ccb88da8601d386be5f668ddf801a83baf
omegaabdullah <abdullah@sudofy.com>
2019-03-30 01:42:15 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'animate-on-scroll-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'animate-on-scroll';
+}
